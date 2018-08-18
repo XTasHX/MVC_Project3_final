275084333bc18db479893fb2e07333d1a447d7df
Natassja <natassja.com@gmail.com>
2018-08-17 20:03:24 -0700

    Add project files.

---
+++ MVC_Project3_final/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Project3_final.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
